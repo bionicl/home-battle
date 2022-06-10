f95f6d8eaca1fac158b58e8d0405706b43ec6308
Bionicl <maciejmaj@me.com>
2022-06-10 18:25:52 +0200

    Some layout changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Tabs, Typography } from 'antd';
 import './App.css';
-import { Button } from 'antd';
+
+const { Title, Text, Link } = Typography;
+const { TabPane } = Tabs;
+
 
 function App() {
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
+    <div className="app">
+      <Title level={2}>Bitwa o mieszkanie</Title>
+      <Tabs defaultActiveKey="1">
+        <TabPane tab="Historia" key="1">
+          Content of Tab Pane 1
+        </TabPane>
+        <TabPane tab="Podsumowanie" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+      </Tabs>
     </div>
   );
 }
