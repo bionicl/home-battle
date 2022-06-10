4f9a1ea078ecf37145bf084d28944912058a74cc
Bionicl <maciejmaj@me.com>
2022-06-10 20:48:57 +0200

    Basic MongoDB setup

--- api/HttpTrigger1.cs
+++ api/HttpTrigger1.cs
@@ -7,6 +7,11 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using TealFire.HomeBattle.Models;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using System.Collections.Generic;
 
 namespace TealFire.HomeBattle
 {
@@ -17,19 +22,20 @@ namespace TealFire.HomeBattle
 				[HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
 				ILogger log)
 		{
-			log.LogInformation("C# HTTP trigger function processed a request.");
+			var client = new MongoClient(Environment.GetEnvironmentVariable("mongoDBURL", EnvironmentVariableTarget.Process));
+			var database = client.GetDatabase("db");
+			var collection = database.GetCollection<LogRow>("log");
+			var documents = await collection.Find(s => s.notes != "" || s.notes == "").ToListAsync();
 
-			string name = req.Query["name"];
 
-			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-			dynamic data = JsonConvert.DeserializeObject(requestBody);
-			name = name ?? data?.name;
-
-			string responseMessage = string.IsNullOrEmpty(name)
-					? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-					: $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-			return new OkObjectResult(responseMessage);
+			string output = "";
+			foreach (var item in documents)
+			{
+				// BsonClassMap.RegisterClassMap<MyClass>();
+				output += JsonConvert.SerializeObject(item) + "\n";
+				// item[]
+			}
+			return new OkObjectResult(output);
 		}
 	}
 }
